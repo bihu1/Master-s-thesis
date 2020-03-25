fadf6f641e36932ed06d6628133823ab7702a6de
Bihuniak <piotr.bihuniak@sagiton.pl>
2020-03-26 00:27:25 +0100

    learning tests

--- functional8p/src/test/java/pl/bihuniak/functional8p/Functional8pApplicationTests.java
+++ functional8p/src/test/java/pl/bihuniak/functional8p/Functional8pApplicationTests.java
@@ -3,11 +3,20 @@ package pl.bihuniak.functional8p;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+//@SpringBootTest
 class Functional8pApplicationTests {
 
     @Test
     void contextLoads() {
+        Stream.of(1,2,3,4,5,0)
+            .map(x -> 10 / x)
+            .peek(System.out::println)
+            .map(x -> 5 / x)
+            .peek(System.out::println)
+            .collect(Collectors.toList());
     }
 
 }
--- reactive8mr/src/test/java/pl/bihuniak/reactive8mr/Reactive8mrApplicationTests.java
+++ reactive8mr/src/test/java/pl/bihuniak/reactive8mr/Reactive8mrApplicationTests.java
@@ -2,12 +2,27 @@ package pl.bihuniak.reactive8mr;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.publisher.Flux;
 
-@SpringBootTest
+//@SpringBootTest
 class Reactive8mrApplicationTests {
+
+    private static String s = "";
 
     @Test
     void contextLoads() {
+        Flux.just(1,2,3,4,5,0)
+            .map(x -> 10 / x)
+            .doOnNext(Reactive8mrApplicationTests::print)
+            .map(x -> 5 / x)
+            .doOnNext(Reactive8mrApplicationTests::print)
+            .subscribe();
     }
 
+    private static Object print(Object o) {
+        s = !s.isEmpty() ? s.concat("->") : s;
+        s = s.concat(o.toString());
+        System.out.println(s);
+        return o;
+    }
 }
